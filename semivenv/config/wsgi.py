2a069a1b2252fdc1f845574c28721543281d5948
jo-soobin <jsb802@naver.com>
2022-10-05 14:18:00 +0900

    upload semivenv

---
+++ semivenv/config/wsgi.py
@@ -0,0 +1,19 @@
+"""
+WSGI config for config project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+import sys
+import os
+from django.core.wsgi import get_wsgi_application
+sys.path.append('C:/projects')
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+application = get_wsgi_application()
