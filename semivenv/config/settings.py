c2057afff77e092319390089075f36d362725fd1
Soobin, Jo <90607293+jo-soobin@users.noreply.github.com>
2022-10-05 21:41:34 +0900

    Update settings.py

--- semivenv/config/settings.py
+++ semivenv/config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o=_@^=8*k=a6c%f*dqn0zr-m2(h6(7t!(6cx1z!(h=0el-r+5g'
+SECRET_KEY = 'django-insecure-o=[]'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
