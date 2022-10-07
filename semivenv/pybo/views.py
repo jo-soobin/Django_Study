eee14a1069300fba337175c81c3388e38bacd3cf
zzeeye <zzeeye95@gmail.com>
2022-10-07 15:49:03 +0900

    media directory path add

--- semivenv/pybo/views.py
+++ semivenv/pybo/views.py
@@ -3,5 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 def index(request):
+    
+
+    '''
+    수빈, 지예 개발
+
+    '''
+
     context = {'question_list': 'question_list'}
+
     return render(request, 'pybo/main.html', context)
