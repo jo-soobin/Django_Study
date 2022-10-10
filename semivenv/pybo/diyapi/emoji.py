ce8223cc479b828ab4273de12aa7044a4423399d
zzeeye <zzeeye95@gmail.com>
2022-10-11 00:32:38 +0900

    test

---
+++ semivenv/pybo/diyapi/emoji.py
@@ -0,0 +1,17 @@
+# 3. 리뷰 예측
+
+# 모델 불러오기
+import re
+from konlpy.tag import Okt
+import pickle
+import random
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# import tensorflow
+
+import pandas
+
+# 감정 분류 및 이모티콘 랜덤 출력 함수
+def sentiment_predict(new_sentence):
+  return 1
